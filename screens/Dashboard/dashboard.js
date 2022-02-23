71c0c167b00a10765d7e2348b5d849aaaea89479
Larry Feldman <lfeldman65@gmail.com>
2022-02-22 19:49:51 -0800

    Link dashboard to pages.

--- screens/Dashboard/dashboard.js
+++ screens/Dashboard/dashboard.js
@@ -4,6 +4,7 @@ import MenuIcon from '../../components/menuIcon';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
 import { Analytics, PageHit, Event } from 'expo-analytics';
+import {useState} from "react"; 
 
 import callImage from '../../assets/quickCalls.png';
 import noteImage from '../../assets/quickNotes.png';
@@ -14,77 +15,20 @@ import goalsImage from '../../assets/quickGoals.png';
 import transImage from '../../assets/quickTrans.png';
 import todoImage from '../../assets/quickToDo.png';
 import calendarImage from '../../assets/quickCalendar.png';
-//import { call } from 'react-native-reanimated';
 
 const analytics = new Analytics('UA-65596113-1');
 /*analytics.hit(new PageHit('Home'))
   .then(() => console.log("success"))
   .catch(e => console.log(e.message)); */
 
-function CallsPressed() {
-  console.log('Calls Pressed');
-  analytics.event(new Event('Dashboard', 'Calls Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
 
-function NotesPressed() {
-  console.log('Notes Pressed');
-  analytics.event(new Event('Dashboard', 'Notes Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function PopPressed() {
-  console.log('Pop Pressed');
-  analytics.event(new Event('Dashboard', 'Pop-Bys Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function PACPressed() {
-  console.log('PAC Pressed');
-  analytics.event(new Event('Dashboard', 'PAC Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function RelPressed() {
-  console.log('Rel Pressed');
-  analytics.event(new Event('Dashboard', 'Relationships Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function GoalsPressed() {
-  console.log('Goals Pressed');
-  analytics.event(new Event('Dashboard', 'Goals Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function TransPressed() {
-  console.log('Transactions Pressed');
-  analytics.event(new Event('Dashboard', 'Transactions Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function ToDoPressed() {
-  console.log('ToDo Pressed');
-  analytics.event(new Event('Dashboard', 'To-Dos Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-function CalendarPressed() {
-  console.log('Calendar Pressed');
-  analytics.event(new Event('Dashboard', 'Calendar Button', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
-}
-
-export default function App() {
+export default function Dashboard(props) {
+  //const { navigation } = props;
+  const [expanded, setExpanded] = useState({
+    relationships: false,
+    transactions: false,
+  });
+  
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -93,24 +37,54 @@ export default function App() {
     });
   });
 
+  const pressedCalls = (screenName) => 
+  {
+    console.log(screenName);
+    analytics.event(new Event('Dashboard', 'Calls', 'Pressed', 0))
+  	navigation.navigate(screenName)
+  }
+
+  const pressedNotes = (screenName) => 
+  {
+    console.log(screenName);
+    analytics.event(new Event('Dashboard', 'Notes', 'Pressed', 0))
+  	navigation.navigate(screenName)
+  }
+
+  const otherPressed = (screenName) => 
+  {
+    console.log(screenName);
+    var prettyName = screenName;
+    if(screenName == "manageRelationships") 
+    {
+      prettyName = "Relationships";
+    }
+    else if(screenName == "realEstateTransactions") 
+    {
+      prettyName = "Transactions";
+    }
+    analytics.event(new Event('Dashboard', prettyName, 'Pressed', 0))
+  	navigation.navigate(screenName)
+  }
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.row}>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={CallsPressed}>
+          <TouchableOpacity onPress={() => pressedCalls('PAC')}>
               <Image source={callImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Calls</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={NotesPressed}>
+          <TouchableOpacity onPress={() => pressedNotes('PAC')}>
               <Image source={noteImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Notes</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={PopPressed}>
+          <TouchableOpacity onPress={() => otherPressed('Pop-Bys')}>
               <Image source={popImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Pop-Bys</Text>
@@ -118,7 +92,7 @@ export default function App() {
         </View>
         <View style={styles.row}>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={PACPressed}>
+          <TouchableOpacity onPress={() => otherPressed('PAC')}>
               <Image source={pacImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Priority</Text>
@@ -126,13 +100,13 @@ export default function App() {
             <Text style={styles.names}>Center</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={RelPressed}>
+          <TouchableOpacity onPress={() => otherPressed('manageRelationships')}>
               <Image source={relImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Relationships</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={GoalsPressed}>
+          <TouchableOpacity onPress={() => otherPressed('Goals')}>
               <Image source={goalsImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Goals</Text>
@@ -140,19 +114,19 @@ export default function App() {
         </View>
         <View style={styles.row}>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={TransPressed}>
+          <TouchableOpacity onPress={() => otherPressed('realEstateTransactions')}>
               <Image source={transImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Transactions</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={ToDoPressed}>
+          <TouchableOpacity onPress={() => otherPressed('To-Do')}>
               <Image source={todoImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>To-Do's</Text>
           </View>
           <View style={styles.pair}>
-            <TouchableOpacity onPress={CalendarPressed}>
+          <TouchableOpacity onPress={() => otherPressed('Calendar')}>
               <Image source={calendarImage} style={styles.logo} />
             </TouchableOpacity>
             <Text style={styles.names}>Calendar</Text>
