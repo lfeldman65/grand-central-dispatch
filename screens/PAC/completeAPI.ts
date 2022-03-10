127126c393694663285fb20bb310094b419e6c98
Larry Feldman <lfeldman65@gmail.com>
2022-03-09 17:59:49 -0800

    Don't need.

--- screens/PAC/completeAPI.ts
+++
@@ -1,6 +0,0 @@
-import { http } from '../../utils/http';
-import { PACPostponeResponse } from './interfaces';
-
-export function postPACComplete(contactGUID: string): Promise<PACPostponeResponse> {
-  return http.post(`contactsPostponeAction/' + 'contactGUID`);
-}
