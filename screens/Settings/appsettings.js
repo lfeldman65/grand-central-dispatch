6c85ec803b585f0facbcbdc02e1f469036755854
Larry Feldman <lfeldman65@gmail.com>
2022-03-15 18:20:45 -0700

    Trying to put hook in place for dark mode.

--- screens/Settings/appsettings.js
+++ screens/Settings/appsettings.js
@@ -1,48 +1,54 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, Dimensions, Linking } from 'react-native';
 import MenuIcon from '../../components/menuIcon';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
-import { Analytics, PageHit, Event} from 'expo-analytics';
-
-const analytics = new Analytics('UA-65596113-1');
+import { Analytics, PageHit, Event } from 'expo-analytics';
+import { analytics } from '../../constants/analytics';
 
 export default function SettingsScreen(props) {
-
   const navigation = useNavigation();
-
+  const [isDarkMode, setIsDarkMode] = useState(true);
   function signOutPressed() {
     console.log('Sign Out');
-    navigation.navigate('Login')
-    analytics.event(new Event('Settings', 'Sign Out', 'Pressed', 0))
-    .then(() => console.log("button success"))
-    .catch(e => console.log(e.message));
+    navigation.navigate('Login');
+    analytics
+      .event(new Event('Settings', 'Sign Out', 'Pressed', 0))
+      .then(() => console.log('button success'))
+      .catch((e) => console.log(e.message));
+  }
+
+  function changeBackground() {
+    console.log(isDarkMode);
+    setIsDarkMode(!isDarkMode);
   }
 
   useEffect(() => {
     navigation.setOptions({
-      headerLeft: (props) => (<MenuIcon />)
+      headerLeft: (props) => <MenuIcon />,
     });
   });
 
   return (
-    <TouchableOpacity onPress={signOutPressed}>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-         <Text style={styles.signOutText}>Sign Out</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={{ flex: 1, backgroundColor: { isDarkMode }, alignItems: 'center' }}>
+      <TouchableOpacity onPress={signOutPressed}>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </View>
+      </TouchableOpacity>
 
+      <TouchableOpacity onPress={changeBackground}>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={styles.buttonText}>Toggle Dark Mode</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#004F89',
-    alignItems: 'center',
-  },
-  signOutText: {
+  buttonText: {
     marginTop: 20,
-    color: 'blue'
-  }
+    color: 'blue',
+  },
 });
