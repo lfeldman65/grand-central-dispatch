8f4530c9c06dc538bcf85154eb6f184b9da63af2
Larry Feldman <lfeldman65@gmail.com>
2022-08-24 11:27:11 -0700

    API for edit profile.

--- screens/Settings/api.ts
+++ screens/Settings/api.ts
@@ -1,6 +1,40 @@
 import { http } from '../../utils/http';
-import { ProfileDataResponse } from './interfaces';
+import { ProfileDataResponse, EditProfileDataResponse } from './interfaces';
 
 export function getProfileData(): Promise<ProfileDataResponse> {
   return http.get('setup/profile');
+}
+
+export function editProfileData(
+  email: string,
+  businessType: string,
+  timezone: string,
+  mobile: string,
+  firstName: string,
+  lastName: string,
+  companyName: string,
+  street1: string,
+  street2: string,
+  city: string,
+  state: string,
+  zip: string,
+  country: string
+): Promise<EditProfileDataResponse> {
+  return http.post('setup/profile', {
+    body: {
+      email,
+      businessType,
+      timezone,
+      mobile,
+      firstName,
+      lastName,
+      companyName,
+      street1,
+      street2,
+      city,
+      state,
+      zip,
+      country,
+    },
+  });
 }
--- screens/Settings/interfaces.ts
+++ screens/Settings/interfaces.ts
@@ -1,12 +1,37 @@
 // Profile in Postman
 
 export interface ProfileDataResponse {
-  data: ProfileProps;
+  data: ProfileDataProps;
   error: string;
   status: string;
 }
 
-export interface ProfileProps {
+export interface ProfileDataProps {
+  email: string;
+  businessType: string;
+  timezone: string;
+  mobile: string;
+  firstName: string;
+  lastName: string;
+  companyName: string;
+  street1: string;
+  street2: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+  hasBombBombPermission: boolean;
+}
+
+// Edit Profile in Postman
+
+export interface EditProfileDataResponse {
+  data: EditProfileProps;
+  error: string;
+  status: string;
+}
+
+export interface EditProfileProps {
   email: string;
   businesstype: string;
   timezonne: string;
