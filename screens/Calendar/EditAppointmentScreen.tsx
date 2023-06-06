56e4518dabadc41b9d427c7c8e19c3c9f5bf098c
Larry Feldman <lfeldman65@gmail.com>
2023-06-06 11:39:03 -0700

    Fix unable to hide picker.

--- screens/Calendar/EditAppointmentScreen.tsx
+++ screens/Calendar/EditAppointmentScreen.tsx
@@ -133,7 +133,7 @@ export default function EditAppointmentScreen(props: any) {
   }
 
   function hideStartTimePicker() {
-    setShowEndDate(false);
+    setShowStartTime(false);
   }
 
   function hideEndDatePicker() {
