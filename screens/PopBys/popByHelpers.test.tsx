190db62331a7eeef35f34256f31d3493965117b8
Larry Feldman <lfeldman65@gmail.com>
2023-09-20 21:11:04 -0700

    add another popby test.

--- screens/PopBys/popByHelpers.test.tsx
+++ screens/PopBys/popByHelpers.test.tsx
@@ -1,4 +1,5 @@
-import { milesBetween, convertYearlyWeekNumber } from './popByHelpers';
+import { PopByRadiusDataProps, PopByAddress } from './interfaces';
+import { milesBetween, convertYearlyWeekNumber, matchesSearch } from './popByHelpers';
 import { describe, expect, test } from '@jest/globals';
 
 describe('miles between', () => {
@@ -20,5 +21,37 @@ describe('convert yearly week number 2', () => {
   test('checks yearly week number 2', () => {
     var result = convertYearlyWeekNumber('1');
     expect(result).toBe(2);
+  });
+});
+
+describe('matches search 1', () => {
+  var address: PopByAddress = {
+    street: '1730 Valley Oak Way',
+    street2: '',
+    city: 'Oceanside',
+    state: 'CA',
+    zip: '92056',
+    country: 'USA',
+    isFavorite: 'false',
+  };
+  var person: PopByRadiusDataProps = {
+    id: '',
+    firstName: 'Larry',
+    lastName: 'Feldman',
+    email: '',
+    phone: '',
+    mobile: '',
+    homePhone: '',
+    officePhone: '',
+    ranking: '',
+    distance: '',
+    address: address,
+    location: { latitude: '34', longitude: '-117' },
+    lastPopbyDate: '',
+    notified: false,
+  };
+  test('matches search 1', () => {
+    var result = matchesSearch(person, 'Larry');
+    expect(result).toBe(true);
   });
 });
